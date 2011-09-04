0d323d0a1286e84eedf6e9fa28aee41d3ecb2401
dylanmei <dylanmei@gmail.com>
2011-09-04 16:34:05 -0700

    Remove references to test template

--- MailPimp/TemplateModule.cs
+++ MailPimp/TemplateModule.cs
@@ -15,34 +15,39 @@ namespace MailPimp
 			//Get["/{Name}"] = parameters => {
 			//    return Response.AsJson(new {parameters.Name});
 			//};
-			Post["/{name}/deliver"] = parameters => {
+			Post["/{Name}/deliver"] = parameters => {
+				string template = parameters.Name;
 				var model = this.Bind<DeliveryModel>();
-				var sender = GetSender();
-				sender.Send(GetMailbag(model));
+				Sender.Send(GetMailbag(template, model));
 				return HttpStatusCode.OK;
 			};
-			Post["/{name}/mailbag"] = parameters => {
+			Post["/{Name}/mailbag"] = parameters => {
+				string template = parameters.Name;
 				var model = this.Bind<DeliveryModel>();
-				return Response.AsJson(GetMailbag(model));
+				return Response.AsJson(GetMailbag(template, model));
 			};
-			Post["/{name}/display"] = parameters => {
+			Post["/{Name}/display"] = parameters => {
+				string template = parameters.Name;
 				var model = this.Bind<DeliveryModel>();
-				return View["happy.spark", model];
+				return View[template, model];
 			};
 		}
 
-		static IMailSender GetSender()
+		static IMailSender Sender
 		{
-			return new MailSender();
+			get
+			{
+				return new MailSender();
+			}
 		}
 
-		Mailbag GetMailbag(DeliveryModel delivery)
+		Mailbag GetMailbag(string template, DeliveryModel delivery)
 		{
 			var mailbag = new Mailbag(delivery.From, delivery.To, delivery.Subject);
 
 			using (var buffer = new TextStream())
 			{
-				View["happy.spark", delivery].Invoke(buffer);
+				View[template, delivery].Invoke(buffer);
 				mailbag.Contents = buffer.ToString();
 			}
 
