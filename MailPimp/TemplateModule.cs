c239ed4d44d55185aec4b7ee1f0e592c70fa617f
dylanmei <dylanmei@gmail.com>
2011-09-04 09:41:36 -0700

    Spark files treated as site content.

--- MailPimp/TemplateModule.cs
+++ MailPimp/TemplateModule.cs
@@ -1,22 +1,9 @@
 ﻿using System;
-using System.Dynamic;
 using Nancy;
 using Nancy.ModelBinding;
 
 namespace MailPimp
 {
-	public class DynamicViewModel
-	{
-		public object From { get; set; }
-		public ExpandoObject Model { get; set; }
-	}
-
-	public class HappyModel
-	{
-		public string Name { get; set; }
-		public string Value { get; set; }
-	}
-
 	public class TemplateModule : NancyModule
 	{
 		public TemplateModule()
@@ -29,19 +16,10 @@ namespace MailPimp
 				return Response.AsJson(new {parameters.Name});
 			};
 			Post["/{name}/deliver"] = parameters => {
-				//dynamic model = this.Bind<ExpandoObject>();
 				throw new NotImplementedException();
 			};
 			Post["/{name}/render"] = parameters => {
 				var model = this.Bind<EmailModel>();
-				//var viewModel = new DynamicViewModel {
-				//    From = new {Name = model.From.Name, Email = model.From.Email},
-				//    Model = model.Model
-				//};
-				//var viewModel = new HappyModel {
-				//    Name = model.Name,
-				//    Value = model.Value
-				//};
 				return View["happy.spark", model];
 			};
 		}
