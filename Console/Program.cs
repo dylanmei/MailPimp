8ae85941cff1b8ee00d1936da71d1cea20371765
dylanmei <dylanmei@gmail.com>
2011-09-13 08:11:29 -0700

    Roll in a console app for scratch testing.

---
+++ Console/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Dynamic;
+using Autofac;
+using MailPimp.Storage.Amazon;
+using MailPimp.Templates;
+
+namespace MailPimp.Console
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var builder = new ContainerBuilder();
+			builder.RegisterAssemblyTypes(typeof(Template).Assembly)
+				.Except<AmazonRepository>()
+				.AsImplementedInterfaces();
+	    	builder.RegisterInstance(new AmazonRepository("XXX"))
+				.As<ITemplateRepository>();
+			Render(builder.Build());
+			System.Console.ReadLine();
+		}
+
+		static void Render(IContainer container)
+		{
+			var b = container.Resolve<ITemplateBuilder>();
+			using (var stream = new TextStream())
+			{
+				var writer = b.RenderTemplate("muppets", GetModel());
+				writer(stream);
+				System.Console.Write(stream.ToString());
+			}
+		}
+
+		static TemplateModel GetModel()
+		{
+			var model = new TemplateModel();
+			model.From.Email = "kermit@muppets.com";
+			model.From.Name = "Kermit";
+			model.To.Add(new Address { Email = "jim@henson.com", Name = "Jim" });
+			model.Subject = "Message from Kermit via MailPimp";
+
+			model.Model = new ExpandoObject();
+			model.Model.Message = "Hey boss, Miss Piggy is smokin' hot!";
+			model.Model.Muppets = new ExpandoObject[3];
+			model.Model.Muppets[0] = new ExpandoObject();
+			model.Model.Muppets[0].Name = "Fozzie";
+			model.Model.Muppets[1] = new ExpandoObject();
+			model.Model.Muppets[1].Name = "Animal";
+			model.Model.Muppets[2] = new ExpandoObject();
+			model.Model.Muppets[2].Name = "Gonzo";
+
+			return model;
+		}
+	}
+}
---
+++ Console/TextStream.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace MailPimp.Console
+{
+	class TextStream : Stream
+	{
+		readonly StringBuilder builder;
+		readonly Encoding encoding = new UTF8Encoding();
+
+		public TextStream()
+		{
+			builder = new StringBuilder();
+		}
+
+		public override void Write(byte[] buffer, int offset, int count)
+		{
+			builder.Append(encoding.GetString(buffer, offset, count));
+		}
+		
+		public override string ToString()
+		{
+			return builder.ToString();
+		}
+
+		public override bool CanRead
+		{
+			get { return false; }
+		}
+
+		public override bool CanSeek
+		{
+			get { return false; }
+		}
+
+		public override bool CanWrite
+		{
+			get { return true; }
+		}
+
+		public override void Flush()
+		{
+		}
+
+		public override long Length
+		{
+			get { return builder.Length;  }
+		}
+       
+		public override long Position
+		{
+			get { return 0; }
+			set { }
+		}
+		
+		public override int Read(byte[] buffer, int offset, int count)
+		{
+			throw new NotSupportedException();
+		}
+
+		public override long Seek(long offset, SeekOrigin origin)
+		{
+			throw new NotSupportedException();
+		}
+
+		public override void SetLength(long value)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
